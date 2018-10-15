80227ac4c78f2d7bebd45aa207035e71219402e2
taiseidayo <tkapple0827aaa@gmail.com>
2018-10-15 22:12:04 +0900

    ぶりぶり

---
+++ BuriburiJunKtion/BuriburiJunKtion/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BuriburiJunKtion
+//
+//  Created by 加藤　大誠 on 2018/10/15.
+//  Copyright © 2018年 BoostJunKtion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Hello BJK"
+    }
+
+
+}
+
