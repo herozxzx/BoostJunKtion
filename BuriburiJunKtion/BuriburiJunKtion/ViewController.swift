aa0183a94afbbd05a70a2f4310388f1d7ca6a46f
taiseidayo <tkapple0827aaa@gmail.com>
2018-10-15 22:46:56 +0900

    ぶりぶり2

--- BuriburiJunKtion/BuriburiJunKtion/ViewController.swift
+++ BuriburiJunKtion/BuriburiJunKtion/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "Hello BJK"
+        label.text = "金額入力"
+        label2.text = "収入"
+        label3.text = "支出"
     }
     override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
