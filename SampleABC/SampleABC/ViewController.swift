b58f0698b4e3cbce9b48415261ade0655494c0cd
taiseidayo <tkapple0827aaa@gmail.com>
2018-10-15 20:59:11 +0900

    Sample

---
+++ SampleABC/SampleABC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleABC
+//
+//  Created by 加藤　大誠 on 2018/10/15.
+//  Copyright © 2018年 BoostJunKtion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
