3017eda4c43b6cc1f1d47251fc801178fadea638
taiseidayo <tkapple0827aaa@gmail.com>
2018-10-15 22:55:03 +0900

    ぶりぶり3

--- BuriburiJunKtion/BuriburiJunKtion/ViewController.swift
+++ BuriburiJunKtion/BuriburiJunKtion/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label3: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.init(red: 198/255, green: 144/255, blue: 99/255, alpha: 90/100)
         label.text = "金額入力"
         label2.text = "収入"
         label3.text = "支出"
